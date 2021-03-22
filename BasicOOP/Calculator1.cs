6fcf6de7d00d10a219f249f5e371387aeba0d585
kraisorn-ya <kraisorn.dun@gmail.com>
2021-03-22 14:39:48 +0700

    Design-Time

---
+++ BasicOOP/Calculator1.cs
@@ -0,0 +1,33 @@
+﻿public struct Calculator
+{
+    private double _x;
+
+    private double _y;
+    public double x
+    {
+        get { return _x; }
+        set { _x = value; }
+    }
+
+    public double y
+    {
+        get { return _y; }
+        set { _y = value; }
+    }
+    public double Add()
+    {
+        return _x + _y;
+    }
+    public double Subtract()
+    {
+        return _x - _y;
+    }
+    public double Mutiply()
+    {
+        return _x * _y;
+    }
+    public double Divide()
+    {
+        return _x / _y;
+    }
+}
--- BasicOOP/Form1.cs
+++ BasicOOP/Form1.cs
@@ -18,74 +18,19 @@ namespace BasicOOP
         {
             InitializeComponent();
         }
-        int[] arr = { 10,100,20,200,3,40,5,50};
-        string str = "";
 
+ 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int[] arr = { 1, 2, 3, 4, 2, 3, 4, 5 };
-            string str = "";
+            Products<int> p1 = new Products<int>();
+            p1.ProductID = "001";
+            p1.ProductName = "พื้นฐาน Visual Basic";
+            p1.ProductPrice = 345;
 
-            for (int i=0; i < arr.Length; i++)
-            {
-                str = str + arr[i] + " ";
-            }
-
-            str = str + Environment.NewLine
-            + "----------------------------"
-            + Environment.NewLine;
-
-            int[] arrDis = arr.Distinct().ToArray();
-            for (int i = 0; i < arrDis.Length; i++)
-            {
-                str = str + arrDis[i] + " ";
-            }
-
-            MessageBox.Show(str, "result");
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            str = "";
-            for (int i = 0; i < arr.Length; i++)
-            { 
-                str = str + arr[i] + " ";
-            }
-            str = str + Environment.NewLine
-                    + "------------------"
-                    + Environment.NewLine;
-            int max = arr[0];
-            for (int j = 1; j < arr.Length; j ++)
-            {
-                if (max <arr[j])
-                {
-                    max = arr[j];
-                }
-            }
-            str = str + max.ToString();
-            MessageBox.Show(str, "result Max");
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            str = "";
-            for (int i = 0; i < arr.Length; i++)
-            {
-                str = str + arr[i] + " ";
-            }
-            str = str + Environment.NewLine
-                    + "------------------"
-                    + Environment.NewLine;
-            int min = arr[0];
-            for (int j = 1; j < arr.Length; j++)
-            {
-                if (min > arr[j])
-                {
-                    min = arr[j];
-                }
-            }
-            str = str + min.ToString();
-            MessageBox.Show(str, "result Max");
+            Products<double> p2 = new Products<double>();
+            p2.ProductID = "002";
+            p2.ProductName = "พื้นฐาน Visual Basic";
+            p2.ProductPrice = 379.5;
         }
     }
 }
